ad5888b4dbca822d4b740829ef0b59a978be453e
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-04-16 16:35:51 -0700

    fill in schema components

--- src/nmdc_patterns/datamodel/nmdc_patterns.py
+++ src/nmdc_patterns/datamodel/nmdc_patterns.py
@@ -1,5 +1,5 @@
 # Auto generated from nmdc_patterns.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-04-16T16:29:37
+# Generation date: 2024-04-16T16:35:41
 # Schema: nmdc-patterns
 #
 # id: https://w3id.org/sierra-moxon/nmdc-patterns
@@ -22,8 +22,8 @@ from linkml_runtime.utils.formatutils import camelcase, underscore, sfx
 from linkml_runtime.utils.enumerations import EnumDefinitionImpl
 from rdflib import Namespace, URIRef
 from linkml_runtime.utils.curienamespace import CurieNamespace
-from linkml_runtime.linkml_model.types import Date, Integer, String, Uriorcurie
-from linkml_runtime.utils.metamodelcore import URIorCURIE, XSDDate
+from linkml_runtime.linkml_model.types import String, Uriorcurie
+from linkml_runtime.utils.metamodelcore import URIorCURIE
 
 metamodel_version = "1.7.0"
 version = None
@@ -32,11 +32,14 @@ version = None
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
+OBI = CurieNamespace('OBI', 'http://purl.obolibrary.org/obo/OBI_')
 PATO = CurieNamespace('PATO', 'http://purl.obolibrary.org/obo/PATO_')
 BIOLINK = CurieNamespace('biolink', 'https://w3id.org/biolink/')
+DC = CurieNamespace('dc', 'http://purl.org/dc/elements/1.1/')
 EXAMPLE = CurieNamespace('example', 'https://example.org/')
 LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
 NMDC_PATTERNS = CurieNamespace('nmdc_patterns', 'https://w3id.org/sierra-moxon/nmdc-patterns/')
+PROV = CurieNamespace('prov', 'http://www.w3.org/ns/prov#')
 SCHEMA = CurieNamespace('schema', 'http://schema.org/')
 DEFAULT_ = NMDC_PATTERNS
 
@@ -44,82 +47,24 @@ DEFAULT_ = NMDC_PATTERNS
 # Types
 
 # Class references
+class ActivityId(URIorCURIE):
+    pass
+
+
+class WorkflowExecutionActivityId(ActivityId):
+    pass
+
+
 class NamedThingId(URIorCURIE):
     pass
 
 
-class ActivityId(NamedThingId):
+class PlannedProcessId(NamedThingId):
     pass
 
 
-@dataclass
-class NamedThing(YAMLRoot):
-    """
-    A generic grouping for any identifiable entity
-    """
-    _inherited_slots: ClassVar[List[str]] = []
-
-    class_class_uri: ClassVar[URIRef] = SCHEMA["Thing"]
-    class_class_curie: ClassVar[str] = "schema:Thing"
-    class_name: ClassVar[str] = "NamedThing"
-    class_model_uri: ClassVar[URIRef] = NMDC_PATTERNS.NamedThing
-
-    id: Union[str, NamedThingId] = None
-    name: Optional[str] = None
-    description: Optional[str] = None
-
-    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self._is_empty(self.id):
-            self.MissingRequiredField("id")
-        if not isinstance(self.id, NamedThingId):
-            self.id = NamedThingId(self.id)
-
-        if self.name is not None and not isinstance(self.name, str):
-            self.name = str(self.name)
-
-        if self.description is not None and not isinstance(self.description, str):
-            self.description = str(self.description)
-
-        super().__post_init__(**kwargs)
-
-
-@dataclass
-class Activity(NamedThing):
-    """
-    Represents a Activity
-    """
-    _inherited_slots: ClassVar[List[str]] = []
-
-    class_class_uri: ClassVar[URIRef] = NMDC_PATTERNS["Activity"]
-    class_class_curie: ClassVar[str] = "nmdc_patterns:Activity"
-    class_name: ClassVar[str] = "Activity"
-    class_model_uri: ClassVar[URIRef] = NMDC_PATTERNS.Activity
-
-    id: Union[str, ActivityId] = None
-    primary_email: Optional[str] = None
-    birth_date: Optional[Union[str, XSDDate]] = None
-    age_in_years: Optional[int] = None
-    vital_status: Optional[Union[str, "PersonStatus"]] = None
-
-    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self._is_empty(self.id):
-            self.MissingRequiredField("id")
-        if not isinstance(self.id, ActivityId):
-            self.id = ActivityId(self.id)
-
-        if self.primary_email is not None and not isinstance(self.primary_email, str):
-            self.primary_email = str(self.primary_email)
-
-        if self.birth_date is not None and not isinstance(self.birth_date, XSDDate):
-            self.birth_date = XSDDate(self.birth_date)
-
-        if self.age_in_years is not None and not isinstance(self.age_in_years, int):
-            self.age_in_years = int(self.age_in_years)
-
-        if self.vital_status is not None and not isinstance(self.vital_status, PersonStatus):
-            self.vital_status = PersonStatus(self.vital_status)
-
-        super().__post_init__(**kwargs)
+class OmicsProcessingId(PlannedProcessId):
+    pass
 
 
 @dataclass
@@ -134,7 +79,7 @@ class ActivityCollection(YAMLRoot):
     class_name: ClassVar[str] = "ActivityCollection"
     class_model_uri: ClassVar[URIRef] = NMDC_PATTERNS.ActivityCollection
 
-    entries: Optional[Union[Dict[Union[str, ActivityId], Union[dict, Activity]], List[Union[dict, Activity]]]] = empty_dict()
+    entries: Optional[Union[Dict[Union[str, ActivityId], Union[dict, "Activity"]], List[Union[dict, "Activity"]]]] = empty_dict()
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
         self._normalize_inlined_as_dict(slot_name="entries", slot_type=Activity, key_name="id", keyed=True)
@@ -142,31 +87,118 @@ class ActivityCollection(YAMLRoot):
         super().__post_init__(**kwargs)
 
 
+@dataclass
+class Activity(YAMLRoot):
+    """
+    Something that occurs over a period of time and acts upon or with entities; it may include consuming, processing,
+    transforming, modifying, relocating, using, or generating entities.
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = NMDC_PATTERNS["Activity"]
+    class_class_curie: ClassVar[str] = "nmdc_patterns:Activity"
+    class_name: ClassVar[str] = "Activity"
+    class_model_uri: ClassVar[URIRef] = NMDC_PATTERNS.Activity
+
+    id: Union[str, ActivityId] = None
+    was_informed_by: Optional[Union[str, ActivityId]] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.id):
+            self.MissingRequiredField("id")
+        if not isinstance(self.id, ActivityId):
+            self.id = ActivityId(self.id)
+
+        if self.was_informed_by is not None and not isinstance(self.was_informed_by, ActivityId):
+            self.was_informed_by = ActivityId(self.was_informed_by)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class WorkflowExecutionActivity(Activity):
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = NMDC_PATTERNS["WorkflowExecutionActivity"]
+    class_class_curie: ClassVar[str] = "nmdc_patterns:WorkflowExecutionActivity"
+    class_name: ClassVar[str] = "WorkflowExecutionActivity"
+    class_model_uri: ClassVar[URIRef] = NMDC_PATTERNS.WorkflowExecutionActivity
+
+    id: Union[str, WorkflowExecutionActivityId] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.id):
+            self.MissingRequiredField("id")
+        if not isinstance(self.id, WorkflowExecutionActivityId):
+            self.id = WorkflowExecutionActivityId(self.id)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class NamedThing(YAMLRoot):
+    """
+    a databased entity or concept/class
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = NMDC_PATTERNS["NamedThing"]
+    class_class_curie: ClassVar[str] = "nmdc_patterns:NamedThing"
+    class_name: ClassVar[str] = "NamedThing"
+    class_model_uri: ClassVar[URIRef] = NMDC_PATTERNS.NamedThing
+
+    id: Union[str, NamedThingId] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.id):
+            self.MissingRequiredField("id")
+        if not isinstance(self.id, NamedThingId):
+            self.id = NamedThingId(self.id)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass
+class PlannedProcess(NamedThing):
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = OBI["0000011"]
+    class_class_curie: ClassVar[str] = "OBI:0000011"
+    class_name: ClassVar[str] = "PlannedProcess"
+    class_model_uri: ClassVar[URIRef] = NMDC_PATTERNS.PlannedProcess
+
+    id: Union[str, PlannedProcessId] = None
+
+@dataclass
+class OmicsProcessing(PlannedProcess):
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = NMDC_PATTERNS["OmicsProcessing"]
+    class_class_curie: ClassVar[str] = "nmdc_patterns:OmicsProcessing"
+    class_name: ClassVar[str] = "OmicsProcessing"
+    class_model_uri: ClassVar[URIRef] = NMDC_PATTERNS.OmicsProcessing
+
+    id: Union[str, OmicsProcessingId] = None
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.id):
+            self.MissingRequiredField("id")
+        if not isinstance(self.id, OmicsProcessingId):
+            self.id = OmicsProcessingId(self.id)
+
+        super().__post_init__(**kwargs)
+
+
 # Enumerations
-class PersonStatus(EnumDefinitionImpl):
 
-    ALIVE = PermissibleValue(
-        text="ALIVE",
-        description="the person is living",
-        meaning=PATO["0001421"])
-    DEAD = PermissibleValue(
-        text="DEAD",
-        description="the person is deceased",
-        meaning=PATO["0001422"])
-    UNKNOWN = PermissibleValue(
-        text="UNKNOWN",
-        description="the vital status is not known")
-
-    _defn = EnumDefinition(
-        name="PersonStatus",
-    )
 
 # Slots
 class slots:
     pass
 
-slots.id = Slot(uri=SCHEMA.identifier, name="id", curie=SCHEMA.curie('identifier'),
-                   model_uri=NMDC_PATTERNS.id, domain=None, range=URIRef)
+slots.id = Slot(uri=NMDC_PATTERNS.id, name="id", curie=NMDC_PATTERNS.curie('id'),
+                   model_uri=NMDC_PATTERNS.id, domain=None, range=URIRef,
+                   pattern=re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9_\.]+:[a-zA-Z0-9_][a-zA-Z0-9_\-\/\.,]*$'))
 
 slots.name = Slot(uri=SCHEMA.name, name="name", curie=SCHEMA.curie('name'),
                    model_uri=NMDC_PATTERNS.name, domain=None, range=Optional[str])
@@ -174,21 +206,20 @@ slots.name = Slot(uri=SCHEMA.name, name="name", curie=SCHEMA.curie('name'),
 slots.description = Slot(uri=SCHEMA.description, name="description", curie=SCHEMA.curie('description'),
                    model_uri=NMDC_PATTERNS.description, domain=None, range=Optional[str])
 
-slots.primary_email = Slot(uri=SCHEMA.email, name="primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=NMDC_PATTERNS.primary_email, domain=None, range=Optional[str])
-
-slots.birth_date = Slot(uri=SCHEMA.birthDate, name="birth_date", curie=SCHEMA.curie('birthDate'),
-                   model_uri=NMDC_PATTERNS.birth_date, domain=None, range=Optional[Union[str, XSDDate]])
-
-slots.age_in_years = Slot(uri=NMDC_PATTERNS.age_in_years, name="age_in_years", curie=NMDC_PATTERNS.curie('age_in_years'),
-                   model_uri=NMDC_PATTERNS.age_in_years, domain=None, range=Optional[int])
-
-slots.vital_status = Slot(uri=NMDC_PATTERNS.vital_status, name="vital_status", curie=NMDC_PATTERNS.curie('vital_status'),
-                   model_uri=NMDC_PATTERNS.vital_status, domain=None, range=Optional[Union[str, "PersonStatus"]])
+slots.was_informed_by = Slot(uri=NMDC_PATTERNS.was_informed_by, name="was_informed_by", curie=NMDC_PATTERNS.curie('was_informed_by'),
+                   model_uri=NMDC_PATTERNS.was_informed_by, domain=Activity, range=Optional[Union[str, ActivityId]], mappings = [PROV["wasInformedBy"]])
 
 slots.activityCollection__entries = Slot(uri=NMDC_PATTERNS.entries, name="activityCollection__entries", curie=NMDC_PATTERNS.curie('entries'),
                    model_uri=NMDC_PATTERNS.activityCollection__entries, domain=None, range=Optional[Union[Dict[Union[str, ActivityId], Union[dict, Activity]], List[Union[dict, Activity]]]])
 
-slots.Activity_primary_email = Slot(uri=SCHEMA.email, name="Activity_primary_email", curie=SCHEMA.curie('email'),
-                   model_uri=NMDC_PATTERNS.Activity_primary_email, domain=Activity, range=Optional[str],
-                   pattern=re.compile(r'^\S+@[\S+\.]+\S+'))
+slots.WorkflowExecutionActivity_id = Slot(uri=NMDC_PATTERNS.id, name="WorkflowExecutionActivity_id", curie=NMDC_PATTERNS.curie('id'),
+                   model_uri=NMDC_PATTERNS.WorkflowExecutionActivity_id, domain=WorkflowExecutionActivity, range=Union[str, WorkflowExecutionActivityId],
+                   pattern=re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9_\.]+:[a-zA-Z0-9_][a-zA-Z0-9_\-\/\.,]*$'))
+
+slots.Activity_id = Slot(uri=NMDC_PATTERNS.id, name="Activity_id", curie=NMDC_PATTERNS.curie('id'),
+                   model_uri=NMDC_PATTERNS.Activity_id, domain=Activity, range=Union[str, ActivityId],
+                   pattern=re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9_\.]+:[a-zA-Z0-9_][a-zA-Z0-9_\-\/\.,]*$'))
+
+slots.OmicsProcessing_id = Slot(uri=NMDC_PATTERNS.id, name="OmicsProcessing_id", curie=NMDC_PATTERNS.curie('id'),
+                   model_uri=NMDC_PATTERNS.OmicsProcessing_id, domain=OmicsProcessing, range=Union[str, OmicsProcessingId],
+                   pattern=re.compile(r'^[a-zA-Z0-9][a-zA-Z0-9_\.]+:[a-zA-Z0-9_][a-zA-Z0-9_\-\/\.,]*$'))
